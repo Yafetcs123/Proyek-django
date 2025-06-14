460ffd48b7cf742366d84d41ad693d34ed95f645
Yafetcs123 <yafetperfecto@gmail.com>
2025-06-15 01:01:04 +0700

    first commit

--- projek/views.py
+++ projek/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     context = {
         'title':'hallo'
     }
-    return render(request, 'index.html', context)
+    template_name = None
+    if request.user.is_authenticated:
+       template_name = 'user.html'
+    else:
+        template_name = 'ano.html'
+    return render(request, template_name, context)
 
 def Login(request):
-    user = None
     if request.method == 'POST':
         username_input = request.POST['username']
         password_input = request.POST['password']
@@ -21,9 +26,11 @@ def Login(request):
     context = {
         'title':'hallo'
     }
-    return render(request, 'login.html', context)
-
-
+    if request.user.is_authenticated:
+           return redirect('index')
+    else:
+        return render(request, 'login.html', context)
+@login_required
 def logoutt(request):
    if request.method == 'POST':
        if request.POST["logout"] == "Submit":
